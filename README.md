f048c584018f01d8911bf495bf09d6931c241c59
Juan Ignacio Navarro <ju.navarro@estudiantec.cr>
2024-05-06 03:07:37 -0600

    Add the blockchain and simulation files

--- README.md
+++ README.md
@@ -1,1 +1,60 @@
-# blockchain-based-ra
+# Blochain Based Remote Attestation
+
+In this repository you will find the code implemented for a remote attestation that communicates devices via blockchain.
+
+### Blockchain
+
+The blockchain infrastructure is located in the Blockchain/ folder, to use the blockchain use the following commands.
+
+To execute the docker containers needed run the following docker compose command.
+
+```
+docker-compose -f test.yml up
+```
+
+After this, to configure the system and upload the configuration to Sawtooth, the following command should be executed to begin the administrator container and load the admin client information.
+
+```
+docker exec -it administration-client bash
+
+python3 administration.py loadSystemConfig ¬¬ python3 administration.py loadDeviceDB
+```
+
+Now to execute the attesteaion, the mes container should run, use the following commands as an example.
+
+```
+docker exec -it mes bash
+python3 attmgr.py checkRequest 073B
+```
+
+### Simulation
+
+First install the requierements for running the simulation
+
+```
+pip3 install -r requirements.txt
+```
+
+Now you can run the simulation like this
+
+```
+python3 simulator.py <simType> <simRepetitions> <simDuration> <rate>
+```
+
+This is the meaning of the parameters:
+
+- **simType**: The type of simulation (setup, run100 (iterations unit 100% hit rate), hitrate, maxquery)
+
+- **simRepetitions**: Repetitions of the siumlation for run100 . The maximum should be defined here.
+
+- **sumDuration**: Maximum duration of each iteration of the simunlation.
+
+- **rate**: defines the query rate.
+
+For example:
+
+```
+python3 simulator.py hitrate 1 1 5
+```
+ 
+
---
+++ Simulation/simulator.py
@@ -0,0 +1,166 @@
+import sys
+
+
+import networkx as nx
+
+import time
+import graph_search
+from random import randint
+
+
+def validate(evidences, query, timeFunction, minReliability):
+    if evidences[query] is not None:
+        x = time.time() - evidences[query]
+        # print(x)
+        timeFunc = timeFunction[0]
+        xmin = timeFunction[1]
+        xmax = timeFunction[2]
+
+        assert xmin <= xmax
+        if (x <= xmin):
+            return True
+        elif (xmin < x <= xmax):
+            # parse and evaluate time function: eval() does not need to be sanitized, administration transaction family input only by administrators
+            trustScore = eval(graph_search._dedent_string(timeFunc))
+            if trustScore < minReliability:
+                # delete evidence
+                evidences[query] = None
+                return False
+            else:
+                return True
+        else:
+            evidences[query] = None
+            return False
+    else:
+        return False
+
+
+if __name__ == '__main__':
+    minHit = 70
+    args = sys.argv[1:]
+    if(len(args) != 5):
+        print("Please provide args in following order: <systemType> <simType> <simRepetitions> <simDuration> <rate>")
+    systemType = args[0]
+    simType = args[1]
+    simRepetitions = int(args[2])
+    simulationDuration = int(args[3])
+    rate = int(args[4])
+    G = nx.DiGraph()
+    # initialization
+    # maximum searchdepth
+    securityParameter = 6
+
+    minReliability = 0.80
+
+    timeFunction = ['-0.0006666667*x + 1.2', 300, 600]
+
+    nArray = [100, 200, 400, 600, 800, 1000]
+    nArray2 = [2000, 3000, 4000, 5000, 7500, 10000, 15000, 20000, 25000]
+
+    evidenceList = {}
+
+    trustQueryHits = 0
+    trustQueryMisses = 0
+    attestations = 0
+    for i in range(0, 6):
+        n = nArray[i]
+        # maximum number oft trustQueries per second
+        trustQueryRate = n / rate
+        evidence = [None] * n
+        evidenceList = {}
+        pending = [None] * n
+        G.clear()
+        for j in range(0, simRepetitions):
+
+            simulationStart = time.time()
+            lastSecond = time.time()
+            secondIterations = 0
+            while time.time() < (simulationStart + simulationDuration):
+
+                currentSecond = time.time()
+                if ((currentSecond - lastSecond) < 1) and (secondIterations < trustQueryRate):
+                    secondIterations += 1
+                    verifier = randint(0, n - 1)
+                    prover = randint(0, n - 1)
+                    while verifier == prover:
+                        verifier = randint(0, n - 1)
+                        prover = randint(0, n - 1)
+                    if systemType == "legiot":
+                        pathFound, finalRating, entryPoint, path = graph_search.findOptimalPath(prover, verifier,
+                                                                                                securityParameter,
+                                                                                                minReliability,
+                                                                                                evidenceList,
+                                                                                                timeFunction)
+                    else:
+                        pathFound = validate(evidence, prover, timeFunction, minReliability)
+                        entryPoint = None
+                    if pathFound:
+                        trustQueryHits += 1
+                    else:
+                        trustQueryMisses += 1
+                        newEntry = [verifier, entryPoint, time.time()]
+                        testEntry = [prover,time.time()]
+                        if systemType == "centralized":
+                            if entryPoint in evidenceList:
+                                tempList = evidenceList.get(entryPoint)
+                                tempList.append(newEntry)
+                                evidenceList[entryPoint] = tempList
+                            else:
+                                newList = [newEntry]
+                                evidenceList[entryPoint] = newList
+                        else:
+                     
+                            evidence[testEntry[0]] = testEntry[1]
+                elif (currentSecond - lastSecond) >= 1:
+                    lastSecond = currentSecond
+                    secondIterations = 0
+                else:
+                    currentSecond = time.time()
+                    sleepTime = (1 - (currentSecond - lastSecond))
+                    time.sleep(sleepTime)
+            if simType == "run100":
+                if trustQueryMisses == 0:
+                    print("Report -----------------------------------------------------------------")
+                    print("Simulation type", systemType)
+                    print("Iterations: ", j)
+                    print("Number of Nodes:", n)
+                    print("Report -----------------------------------------------------------------")
+                    G.clear()
+                    break
+                elif j == simRepetitions - 1:
+                    print("Report -----------------------------------------------------------------")
+                    print("NO HITS")
+                    print("Simulation type", systemType)
+                    print("Iterations: ", j)
+                    print("Number of Nodes:", n)
+                    print("Report -----------------------------------------------------------------")
+                    break
+                print(trustQueryHits+trustQueryMisses)
+                print("Iteration is finished !!! -----------------------------------------")
+            elif simType == "setup":
+                if trustQueryHits / (trustQueryHits + trustQueryMisses) * 100 >= minHit:
+                    print("Report -----------------------------------------------------------------")
+                    print("Simulation type", systemType)
+                    print("Number of Nodes:", n)
+                    print("Reached a minimum hitrate of %s in %s iterations: " % (minHit, j))
+                    print("Size of graph", len(G.edges))
+                    print("Report -----------------------------------------------------------------")
+                    G.clear()
+                    break
+            else:
+                print("Report -----------------------------------------------------------------")
+                print("Simulation type", systemType)
+                print("Number of Nodes:", n)
+                print("Security Parameter:", securityParameter)
+                print("Minimal Reliability:", minReliability)
+                print("Time Function:", timeFunction)
+                print("Simulation Duration:", simulationDuration)
+                print("Number of TrustQueries:", (trustQueryHits + trustQueryMisses))
+                print("Actual Trust Query Rate:", (trustQueryHits + trustQueryMisses) / (time.time() - simulationStart))
+                print("Hits:", trustQueryHits)
+                print("Misses:", trustQueryMisses)
+                print("HitPercentage:", (trustQueryHits / (trustQueryHits + trustQueryMisses)) * 100)
+                print("Simulation Duration:", (time.time() - simulationStart))
+                print("Report -----------------------------------------------------------------")
+            trustQueryHits = 0
+            trustQueryMisses = 0
